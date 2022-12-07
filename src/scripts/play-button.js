fc575ffb0b2ab4b6b101ea93e217d6ea968c694b
Ziek <zieksanchez3@gmail.com>
2022-12-07 13:00:05 -0600

    edits

--- src/scripts/play-button.js
+++ src/scripts/play-button.js
@@ -3,9 +3,18 @@ let videos = document.getElementsByClassName("mediaItem-video");
 
 Array.from(videoButtons).forEach((button, index) => {
   button.addEventListener("click", (e) => {
+    stopVideos();
     let video = videos[index];
     video.setAttribute("controls", true);
     video.play();
     e.target.style.display = "none";
   });
 });
+
+const stopVideos = () => {
+  Array.from(videos).forEach((video, index) => {
+    video.pause();
+    video.removeAttribute("controls");
+    videoButtons[index].style.display = "block";
+  });
+};

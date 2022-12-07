56b4072c35df763f2a30672a53d193e4f8f291ec
Ziek <zieksanchez3@gmail.com>
2022-12-07 14:27:15 -0600

    edits

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPassthroughCopy("./src/scripts");
